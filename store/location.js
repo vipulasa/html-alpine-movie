56ddcb1c91726acb116cb4f87ae7bba0338b0598
Vipula S Anandapiya <v.s.anandapiya@gmail.com>
2023-07-27 12:46:10 +0530

    code updates

--- store/location.js
+++ store/location.js
@@ -7,7 +7,26 @@ document.addEventListener('alpine:init', () => {
         phone : '+94 11 236 6356',
         seats: 300,
         timeSlots: [
-            
+            {
+                start: '10:30',
+                end: '13:00'
+            },
+            {
+                start: '13:30',
+                end: '16:00'
+            },
+            {
+                start: '16:30',
+                end: '18:30'
+            },
+            {
+                start: '19:00',
+                end: '21:30'
+            },
+            {
+                start: '22:00',
+                end: '00:30'
+            }
         ]
     })
 })
